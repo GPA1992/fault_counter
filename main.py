bce14ea7ea74a48d78d5ac92319212b279800bf6
GPA1992 <gabrielproencaaraujo@gmail.com>
2023-08-08 09:29:06 -0300

    com ambiente criado

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import csv
 
-    
+with open("08-08-2023-unico.csv", encoding="utf8") as file:
+    graduacao_reader = csv.reader(file, delimiter=",", quotechar='"')
+    # Usando o conceito de desempacotamento
+    header, *data = graduacao_reader
+
+print(data[0])
