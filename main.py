a20287b84e4e7cfe816bc370c10281b9e26f1209
GPA <gabrielproencaaraujo@gmail.com>
2023-08-29 20:42:01 -0300

    logica funcionando

--- main.py
+++ main.py
@@ -1,71 +1,66 @@
 import csv
 import os
 from import_all import import_csv_files
+import json
+
+with open("todos_alunos.json", "r") as arquivo_json:
+    todos_alunos = json.load(arquivo_json)
+
 
 all_day_count = import_csv_files("./all_days")
 
-
-todos_alunos_path = "todos_alunos.csv"
-""" chamada_path = "05_10_2023_unica.csv" """
-
-""" parts = chamada_path.split("_")
-tipo_dia = parts[-1].split(".")[0]
-
-dia = os.path.splitext(chamada_path)[0] """
+days_whit_presenca = []
 
 
-todos_alunos = []
-
-
-def read_todos_alunos():
-    with open(todos_alunos_path, encoding="utf8") as file:
-        graduacao_reader = csv.reader(file, delimiter=",", quotechar='"')
-        next(graduacao_reader)
-        for row in graduacao_reader:
-            todos_alunos.append(row[0])
-
-
-def chamada(chamada_path):
-    alunos = []
-    with open(chamada_path, encoding="utf8") as file:
-        graduacao_reader = csv.reader(file, delimiter=",", quotechar='"')
-        next(graduacao_reader)
-        for row in graduacao_reader:
-            alunos.append(row[0])
-
-
-read_todos_alunos()
-""" chamada() """
-
-
-def contador_presenca(chamada_path, alunos):
+def contador_presenca(chamada_path, alunos_presenca):
     parts = chamada_path.split("_")
     tipo_dia = parts[-1].split(".")[0]
     dia = os.path.splitext(chamada_path)[0]
-
-    output_file_path = "todos_alunos_com_presenca.csv"
-
-    with open(todos_alunos_path, encoding="utf8") as input_file, open(
-        output_file_path, "w", encoding="utf8"
-    ) as output_file:
-        reader = csv.reader(input_file)
-        writer = csv.writer(output_file)
-
-        header = next(reader)
-        if dia not in header:
-            header.append(dia)
-
-        writer.writerow(header)
-        for aluno in reader:
-            if aluno[0] in alunos:
-                aluno.append(0)
+    presenca = []
+    for aluno in todos_alunos:
+        if aluno["aluno"] in alunos_presenca:
+            presenca.append((aluno["aluno"], 0))
+        else:
+            if tipo_dia == "unica":
+                presenca.append((aluno["aluno"], 1))
             else:
-                if tipo_dia == "unica":
-                    aluno.append(1)
-                else:
-                    aluno.append(2)
-            writer.writerow(aluno)
+                presenca.append((aluno["aluno"], 2))
+    days_whit_presenca.append((dia, presenca))
 
 
 for day in all_day_count:
     contador_presenca(day[0], day[1])
+
+
+with open("teste.csv", "w", newline="") as arquivo_csv:
+    escritor = csv.writer(arquivo_csv)
+
+
+all_students = set()
+for _, student_data in days_whit_presenca:
+    for student, _ in student_data:
+        all_students.add(student)
+
+# Crie o cabeçalho do CSV
+header = ["Aluno", "Matrícula"]
+for date, _ in days_whit_presenca:
+    header.append(date)
+
+# Crie um dicionário para armazenar os dados dos alunos
+student_dict = {
+    student: {"Aluno": student, "Matrícula": ""} for student in all_students
+}
+
+# Preencha o dicionário com as informações de cada data
+for date, student_data in days_whit_presenca:
+    for student, matricula in student_data:
+        student_dict[student][date] = matricula
+
+# Escreva os dados no arquivo CSV
+with open("output.csv", "w", newline="") as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=header)
+    writer.writeheader()
+    for student_data in student_dict.values():
+        writer.writerow(student_data)
+
+print("Arquivo CSV criado com sucesso!")
